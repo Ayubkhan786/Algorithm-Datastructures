8c34d3649607bf447290a5abf634e518e9a5137d
your Name <mohamedabdulayubm@gmail.com>
2022-07-14 22:39:59 +0530

    [Ayub] Prime Number

--- Algorithm and DataStructures/Program.cs
+++ Algorithm and DataStructures/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Prime Numbers");
+﻿using Algorithm_and_DataStructures;
+
+
+Console.WriteLine("Prime Numbers");
+
+
+
+AlgorithmDataStructures algorithmDataStructures = new AlgorithmDataStructures();
+algorithmDataStructures.DisplayPrime();
